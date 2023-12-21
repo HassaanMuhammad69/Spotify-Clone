0d046490271db7bfe41542747c791026a2a6128a
Hassaan Muhammad <hassaans299@gmail.com>
2023-12-21 07:19:18 -0800

    songs are being displayed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,12 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
 
-export default function Home() {
+export default async function Home() {
+const songs =await getSongs()
+
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto" >
       <Header>
@@ -25,9 +29,7 @@ export default function Home() {
           </h1>
         </div>
 
-        <div>
-          List of songs!
-        </div>
+        <PageContent songs={songs}/>
 
       </div>
     </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:[
+            "devbfcgxegzakrmufffx.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
